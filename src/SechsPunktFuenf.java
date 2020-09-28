e9dfbbb90b41eb6027f4defdd2b6b59d8ad7d3c4
Rashad Khoshnow <rashad.khoshnow@gmail.com>
2020-09-28 09:18:19 +0200

    Array.ToString hinzugefügt

--- src/SechsPunktFuenf.java
+++ src/SechsPunktFuenf.java
@@ -1,34 +1,27 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class SechsPunktFuenf {
     public static void main(String[] args) {
-        int arr[] = new int[10], max, min;
-        double tmp;
-
-        for (int i = 0; i < arr.length; i++) {
-            tmp = 100 * Math.random();
-            tmp = Math.floor(tmp);
-            arr[i] = (int) tmp;
-        }
-
-        System.out.println("unsortiert");
-        for (int i = 0; i < arr.length; i++)
-            System.out.print(arr[i]);
-
-
-        int temp;
-        for (int i = 1; i < arr.length; i++)
-            for (int j = arr.length - 1; j >= i; j--)
-                if (arr[j - 1] > arr[j]) {
-                    temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = temp;
-                }
-
-        System.out.println("sortiert");
-        for (int i = 0; i < arr.length; i++)
-            System.out.println(arr[i]);
-
+        int[] array = {10, 5, 46, 3, 15};
+        System.out.println(Arrays.toString(array));
+        sort(array);
+        String sorted = Arrays.toString(array);
+        System.out.println(sorted);
     }
 
+    public static int[] sort(int[] array) {
+        int temp = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j] < array[j - 1]) {
+                    temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+            String sorted = Arrays.toString(array);
+            System.out.println(sorted);
+        }
+        return array;
+    }
 }
